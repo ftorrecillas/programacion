345a8bd80de3592335d1224d7e480925df532cfa
ftorrecillas <dam.ptorrecillas@institutoserpis.org>
2016-02-12 13:05:29 +0100

    hola mundo practica

--- Ficheros/src/Ficheros4/Alumno.java
+++ Ficheros/src/Ficheros4/Alumno.java
@@ -14,7 +14,7 @@ public class Alumno {
 
 
     private String carnet, nombre, apellidos;
-    private int nota1, nota2, nota3;
+    private double nota1, nota2, nota3;
     Scanner lector = new Scanner(System.in);
     ArrayList<Alumno> listadato=new ArrayList<Alumno>();
     File f = new File("alumno.dat");
@@ -24,12 +24,14 @@ public class Alumno {
     FileInputStream fis = null;
     BufferedInputStream bis = null;
     ObjectInputStream ois = null;
+    DataOutputStream dos=null;
+    DataInputStream dis=null;
     public Alumno(){
     
     
     }
 
-    public Alumno(String carnet, String nombre, String apellidos, int nota1, int nota2, int nota3) {
+    public Alumno(String carnet, String nombre, String apellidos, double nota1, double nota2, double nota3) {
         this.carnet = carnet;
         this.nombre = nombre;
         this.apellidos = apellidos;
@@ -37,9 +39,62 @@ public class Alumno {
         this.nota2 = nota2;
         this.nota3 = nota3;
     }
+
+    public String getCarnet() {
+        return carnet;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public double getNota1() {
+        return nota1;
+    }
+
+    public double getNota2() {
+        return nota2;
+    }
+
+    public double getNota3() {
+        return nota3;
+    }
+
+    public Scanner getLector() {
+        return lector;
+    }
+
+    public void setCarnet(String carnet) {
+        this.carnet = carnet;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public void setNota1(int nota1) {
+        this.nota1 = nota1;
+    }
+
+    public void setNota2(int nota2) {
+        this.nota2 = nota2;
+    }
+
+    public void setNota3(int nota3) {
+        this.nota3 = nota3;
+    }
+    
     public void ponerDato(){
           String carnetmetodo, nombremetodo, apellidosmetodo, cadena;
-          int nota1metodo, nota2metodo, nota3metodo;
+          double nota1metodo, nota2metodo, nota3metodo;
      do{     
         System.out.println("Introduzca el numero carnet del nuevo alumno: ");
         carnetmetodo = lector.nextLine();
@@ -48,11 +103,11 @@ public class Alumno {
         System.out.println("Introduzca los apellidos del alumno: ");
         apellidosmetodo = lector.nextLine();
         System.out.println("Introduzca la primera nota: ");
-        nota1metodo = lector.nextInt();
+        nota1metodo = lector.nextDouble();
         System.out.println("Introduzca la segunda nota: ");
-        nota2metodo = lector.nextInt();
+        nota2metodo = lector.nextDouble();
         System.out.println("Introduzca la tercera nota: ");
-        nota3metodo = lector.nextInt();
+        nota3metodo = lector.nextDouble();
         Alumno alumno01=new Alumno(carnetmetodo,nombremetodo,apellidosmetodo,nota1metodo,nota2metodo,nota3metodo);
         listadato.add(alumno01);
         System.out.println("Quieres añadir mas alunmos? ");
@@ -144,11 +199,107 @@ public class Alumno {
     }
     
     public void pasarFichero5(){
+        double media;
+         File f5 = new File("alumno5.dat");
         try{
-            fis = new FileInputStream(f);      
-           bis= new BufferedInputStream(fis); 
+             fos = new FileOutputStream(f5);      
+           bos= new BufferedOutputStream(fos);  
+           dos=new DataOutputStream(bos); 
            
-        }
+           for(int i=0; i<listadato.size();i++){
+               //se necesitan los getters y setter
+               media=((listadato.get(i).getNota1()+listadato.get(i).getNota2()+listadato.get(i).getNota3())/3);
+                    if(media>=5){
+                         dos.writeUTF(listadato.get(i).getCarnet());
+                         dos.writeDouble(media);
+                    
+                    }  
+           }
+               
+           
+        }catch(FileNotFoundException e)
+                 {
+                     System.out.println("Error. Fichero no encontrado.");
+                     System.out.println(e.getMessage());
+	                 
+	        } catch (IOException e)	
+                {
+                    System.out.println("Error al abrir el fichero");
+                    System.out.println(e.getMessage());
+                } catch (Exception e)
+                {
+                    System.out.println("Error general.");
+                    System.out.println(e.getMessage());
+                } finally 
+                {
+                    try 
+                    {
+                        if (dos!=null) 
+                        {
+                            dos.close();
+                        }
+                        
+                    } catch (IOException e) 
+                    {
+                        System.out.println("Error al cerrar eñ fichero.");
+                        System.out.println(e.getMessage());
+                    }
+                }
+    }
+    
+    public void visualizarFichero5(){
+         File f5 = new File("alumno5.dat");
+         String canet;
+         double nota;
+         try{
+            
+           fis = new FileInputStream(f5);      
+           bis= new BufferedInputStream(fis);     
+           dis=new DataInputStream(bis);
+           
+            do{ 
+            canet=dis.readUTF();
+                
+            nota=dis.readDouble();
+              
+        
+          
+                
+            System.out.println(canet);
+              System.out.println(nota);
+              
+        }while(dos!=null);      
+         }catch(FileNotFoundException e){
+                     System.out.println("Error. Fichero no encontrado.");
+                     System.out.println(e.getMessage());
+	                 
+	        } catch (EOFException e)	
+                {
+                    System.out.println("Fin de fichero");
+                    
+                }catch (IOException e)	
+                {
+                    System.out.println("Error al abrir el fichero");
+                    System.out.println(e.getMessage());
+                } catch (Exception e)
+                {
+                    System.out.println("Error general.");
+                    System.out.println(e.getMessage());
+                } finally 
+                {
+                    try 
+                    {
+                        if (dis!=null) 
+                        {
+                            dis.close();
+                        }
+                        
+                    } catch (IOException e) 
+                    {
+                        System.out.println("Error al cerrar eñ fichero.");
+                        System.out.println(e.getMessage());
+                    }
+                }
     }
     
 }
